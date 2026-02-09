31dd325217ee2ad7ebbabfd683dc45924077567e
jadiel07 <jadiel174@hotmail.com>
2026-02-09 00:10:32 -0300

    att vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'  
 import SchoolDetails from './pages/SchoolDetails'
+import React from 'react';
 
 function App() {
   return (
